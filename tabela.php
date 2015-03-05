bb3fec19c80ea5985fca0ce58df279c72f5b1e89
41419634 <41419634@090026-P33S13.macklabs.br>
2015-03-05 12:00:03 -0300

    asdfasdf

---
+++ tabela.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Atividade1</title>
+        <
+    </head>
+    <body>
+       <?php
+       
+       for($i=0; $i<8; $i++)
+       {
+           echo "<tr><br>";
+           for($j=0; $j<3; $j++)
+           {
+               echo "<td> Célula 1 </td> ";
+           }
+       }
+       
+       
+       ?>
+    </body>
+</html>
